0f465d4ca8cc5a7914dc886cdf0b95885f3fbcf3
Chris Gough <13353626+cngough@users.noreply.github.com>
2020-12-26 22:00:34 +0000

    Resolved config/dependency issues in discord-bot

--- discord-bot.py
+++ discord-bot.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import discord
-import os
-from discord.ext import commands 
 import asyncio
-import os
 import datetime
-import time
-import aiohttp
-from io import BytesIO
 import json
+import os
 import random
+import time
+from io import BytesIO
+
+import aiohttp
+import discord
 import youtube_dl
+from discord.ext import commands
+
 import config
 
 ### Global variables
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+ytdl = youtube_dl.YoutubeDL(config.ytdl_format_options)
 youtube_dl.utils.bug_reports_message = lambda: ''
 client = commands.Bot(command_prefix = '!')
 start_time = datetime.datetime.now()
@@ -24,22 +25,22 @@ react_with_fuck = None
 @client.event
 async def on_ready():
     print ('No adventure is complete without Jingle hat and Jingle feet')        
-    channel = client.get_channel(514244679907147776)
+    channel = client.get_channel(config.dev_test)
     global start_time
     start_time = datetime.datetime.now()
     await channel.send('ChairsBot started at: ' + start_time.strftime("%d/%m/%Y, %H:%M:%S"))
 
 @client.event
 async def on_message(message):
-    if setFuck != None:
-        if message.author.name == setFuck:
-            await message.add_reaction(emoji=middleFingerEmoji)
+    if react_with_fuck != None:
+        if message.author.name == react_with_fuck:
+            await message.add_reaction(emoji=config.Emoji.middle_finger)
     if 'horse' in message.content.lower():
-        await message.add_reaction(emoji=hEmoji)
-        await message.add_reaction(emoji=oEmoji)
-        await message.add_reaction(emoji=rEmoji)
-        await message.add_reaction(emoji=sEmoji)
-        await message.add_reaction(emoji=eEmoji)
+        await message.add_reaction(emoji=config.Emoji.regional_indicator_h)
+        await message.add_reaction(emoji=config.Emoji.regional_indicator_o)
+        await message.add_reaction(emoji=config.Emoji.regional_indicator_r)
+        await message.add_reaction(emoji=config.Emoji.regional_indicator_s)
+        await message.add_reaction(emoji=config.Emoji.regional_indicator_e)
     await client.process_commands(message)
 
 @client.command()
@@ -60,23 +61,23 @@ async def changelog(ctx):
 
 @client.command()
 async def fuck(ctx, *args):
-    memberToCheck = ' '.join(args)
-    global setFuck
+    member_to_check = ' '.join(args)
+    global react_with_fuck
     for member in ctx.message.guild.members:
         # print('[DEBUG]' + member.name)
-        if (member.name.lower() == memberToCheck.lower() or (member.nick != None and member.nick.lower() == memberToCheck.lower())):
+        if (member.name.lower() == member_to_check.lower() or (member.nick != None and member.nick.lower() == member_to_check.lower())):
             await ctx.send('That\'s right, fuck ' + member.name)
-            setFuck = member.name
+            react_with_fuck = member.name
             return 
-        if (memberToCheck.lower() in member.name.lower() or (member.nick != None and memberToCheck.lower() in member.nick.lower())):
+        if (member_to_check.lower() in member.name.lower() or (member.nick != None and member_to_check.lower() in member.nick.lower())):
             if member.nick == None:
                 await ctx.send('I\'m guessing you meant fuck ' + member.name + '. If you didn\'t - fuck them anyway!')
             else:
                 await ctx.send('I\'m guessing you meant fuck ' + member.name + '/' + member.nick + '. If you didn\'t - fuck them anyway!')
-            setFuck = member.name
+            react_with_fuck = member.name
             return 
-    await ctx.send('Who the fuck is ' + memberToCheck + '?')
-    setFuck = None
+    await ctx.send('Who the fuck is ' + member_to_check + '?')
+    react_with_fuck = None
 
 @client.command()
 async def dazzyboo(ctx):
@@ -88,9 +89,9 @@ async def serious(ctx):
     await ctx.send(seriousRant)
 
 async def info(ctx, *args):
-    memberToCheck = ' '.join(args)
+    member_to_check = ' '.join(args)
     for member in ctx.message.guild.members:
-        if (member.name.lower() == memberToCheck.lower()):
+        if (member.name.lower() == member_to_check.lower()):
             info_text = """User Information:
             
             * Name: {}, 
@@ -179,7 +180,7 @@ async def god(ctx):
     for x in range(30):
         # Use os.urandom() to generate secure
         godSecure = random.SystemRandom().randint(1, 7569) # 7569 lines in dictionary
-        words = words + godDictionary.get(godSecure).replace("\n", " ")
+        words = words + config.god_dictionary.get(godSecure).replace("\n", " ")
     await ctx.send(words)
 
 async def checkKol():
@@ -193,7 +194,7 @@ async def checkKol():
             x = x.replace('kol-', '')
             x = x.replace('.log', '')
             date_time = datetime.datetime.strptime(x, '%Y%m%d%H%M%S')
-            channel = client.get_channel(514244679907147776)
+            channel = client.get_channel(config.dev_test)
             await channel.send('KoL Cron Job successfully executed at: ' + date_time.strftime("%d/%m/%Y, %H:%M:%S"))
             os.remove(todelete)
         await asyncio.sleep(60) # task runs every 60s
@@ -202,13 +203,13 @@ async def dailyHorse():
     await client.wait_until_ready()
     print("Daily Horse has started")
     while(True):
-        channel = client.get_channel(791301208756584469)
+        channel = client.get_channel(config.daily_horse)
         red = random.SystemRandom().randint(1, 255)
         green = random.SystemRandom().randint(1, 255)
         blue = random.SystemRandom().randint(1, 255)        
         embed = discord.Embed(colour=discord.Colour.from_rgb(red, green, blue))
         session = aiohttp.ClientSession()    
-        response = await session.get('https://api.giphy.com/v1/gifs/random?tag=horse&api_key=' + giphy_token)
+        response = await session.get('https://api.giphy.com/v1/gifs/random?tag=horse&api_key=' + config.giphy_api_key)
         data = json.loads(await response.text())
         embed.set_image(url=data['data']['images']['original']['url'])
         await channel.send('Enjoy your daily horse GIF - \'' + data['data']['title'] + '\' brought to you by: ' + data['data']['username'])
@@ -216,6 +217,6 @@ async def dailyHorse():
         await session.close()
         await asyncio.sleep(86400) # runs every 24 hours
 
-#client.loop.create_task(dailyHorse())
+client.loop.create_task(dailyHorse())
 client.loop.create_task(checkKol())
-client.run(token)
+client.run(config.discord_api_key)
