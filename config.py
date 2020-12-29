c9f433739b327112956302c68e0991ac964e5911
Chris Gough <13353626+cngough@users.noreply.github.com>
2020-12-29 01:02:26 +0000

    Added return statement to base64_to_utf8

--- config.py
+++ config.py
@@ -106,6 +106,7 @@ def base64_to_utf8(base64_message):
        base64_bytes = base64_message.encode('utf-8')
        message_bytes = base64.b64decode(base64_bytes)
        message = message_bytes.decode('utf-8')
+       return message
 
 # Static text fields
 ON_READY = "No adventure is complete without Jingle hat and Jingle feet"
